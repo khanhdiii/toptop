c0315be0d50fb16af7f50f22ecde387132591113
khanhdiii <khanh12369874@gmail.com>
2023-12-06 01:02:30 +0700

    add: ui header search

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Sidebar() {
   return (
-    <aside className="wrapper w-[356px] min-h-[1000px] bg-slate-500">
+    <aside className="wrapper w-[356px] min-h-[1000px">
       <h2>Sidebar</h2>
     </aside>
   );
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import images from '~/assets/images';
+import { AiFillCloseCircle, AiOutlineLoading3Quarters, AiOutlineSearch } from 'react-icons/ai';
 
 function Header() {
   return (
     <header className="wrapper h-[60px] w-full shadow-[0_1px_1px_rgba(0,0,0,0.12)] flex justify-center">
-      <div className="inner h-full w-[1150px] ">as</div>
+      <div className="inner h-full w-[1150px] flex items-center justify-between ">
+        <div className="logo">
+          <img src={images.logo.default} alt="TikTok" width="118" height="42" />
+        </div>
+        <div className="search relative w-[500px] h-[46px] border-solid border-[1.5px] border-transparent bg-gray-200 rounded-[92px] pl-4 flex after:content hover:border-gray-400">
+          <input
+            placeholder="Search"
+            spellCheck={false}
+            className="text-base border-none outline-none bg-transparent text-black h-full flex-1 caret-pink-400"
+          />
+          <button className="clear absolute right-[72px] top-[50%] transform -translate-y-1/2 text-gray-400">
+            <AiFillCloseCircle />
+          </button>
+          <button className="loading absolute right-[72px] top-[50%] transform -translate-y-1/2 text-gray-400">
+            <AiOutlineLoading3Quarters />
+          </button>
+          <button className="search-button w-[52px] h-full border-none outline-none justify-center flex items-center rounded-r-full text-3xl text-gray-400 border-[1.5px] border-transparent hover:bg-gray-300 hover:text-black">
+            <span className="search-line after:absolute after:content after:block after:w-[1px] after:h-[28px] after:top-[9px] after:right-[52px] after:bg-gray-400"></span>
+            <AiOutlineSearch />
+          </button>
+          <div className="action"></div>
+        </div>
+      </div>
     </header>
   );
 }
